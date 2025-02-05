1a2d82668bde73638bea20eb274f388ed8f1e53f
Enache Andrei <enache.andrei.george@gmail.com>
2025-02-05 18:38:13 +0200

    Added settings for render db

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 from pathlib import Path
 import os
 from dotenv import load_dotenv
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -86,16 +87,22 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+# USE THIS FOR CHOREO DB
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': os.getenv("DB_NAME", ""),
+#         'USER': os.getenv("DB_USER", ""),
+#         'PASSWORD': os.getenv("DB_PWD", ""),
+#         'HOST': os.getenv("DB_HOST", ""),
+#         'PORT': os.getenv("DB_PORT", "")
+#     }
+# }
+
+# RENDER DB
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv("DB_NAME", ""),
-        'USER': os.getenv("DB_USER", ""),
-        'PASSWORD': os.getenv("DB_PWD", ""),
-        'HOST': os.getenv("DB_HOST", ""),
-        'PORT': os.getenv("DB_PORT", "")
-    }
-}
+    'default': dj_database_url.config(default=os.getenv("DB_URL", ""),
+                                      conn_max_age=600)}
 
 
 # Password validation

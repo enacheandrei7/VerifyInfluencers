da5a858450db11be19052c456262484e930803a6
Enache Andrei <enache.andrei.george@gmail.com>
2025-02-05 20:00:26 +0200

    Added allowed render hosts

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -31,7 +31,7 @@ PERPLEXITY_API_KEY = os.getenv("PERPLEXITY_API_KEY", "")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["backend-3907805525.dp-development-verifyinfluencersd-57518-3953375731", "*.onrender.com"]
+ALLOWED_HOSTS = ["backend-3907805525.dp-development-verifyinfluencersd-57518-3953375731", "backend-qo6t.onrender.com"]
 
 
 CORS_ALLOWED_ORIGINS = ["http://127.0.0.1:5173", "http://localhost:5173"]
